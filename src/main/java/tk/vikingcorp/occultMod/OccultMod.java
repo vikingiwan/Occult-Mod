c82e9dc1e989e2e6623f611f7b6fe56cf701c1e3
BuildTools <vikingiwan@gmail.com>
2017-03-08 20:17:58 -0500

    Created OccultMod.java

---
+++ src/main/java/tk/vikingcorp/occultMod/OccultMod.java
@@ -0,0 +1,32 @@
+package tk.vikingcorp.occultMod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid=Reference.MODID, name=Reference.NAME, version=Reference.VERSION)
+
+public class OccultMod 
+{
+	//PRE-INIT
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	//INIT
+	@Mod.EventHandler
+	public void Init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	//POST-INIT
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}

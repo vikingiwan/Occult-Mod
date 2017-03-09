83a7f225d7243ee23f3f6e63d1c46d659857c0b6
BuildTools <vikingiwan@gmail.com>
2017-03-08 21:30:38 -0500

    added en_US.lang

---
+++ eclipse/.metadata/.plugins/org.eclipse.core.resources/.history/e8/6005e2036d040017127bcc791336db57
@@ -0,0 +1,8 @@
+package tk.vikingcorp.occultMod.items;
+
+import net.minecraft.item.Item;
+
+public class OccultModItem extends Item
+{
+
+}
--- src/main/java/tk/vikingcorp/occultMod/items/OccultModItem.java
+++ src/main/java/tk/vikingcorp/occultMod/items/OccultModItem.java
@@ -1,8 +1,13 @@
 package tk.vikingcorp.occultMod.items;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class OccultModItem extends Item
 {
-
+	public OccultModItem()
+	{
+		super();
+		setCreativeTab(CreativeTabs.tabMisc);
+	}
 }

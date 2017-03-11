3224a96fc3651ac3a3bc7bc1d7a7bc6e5f6047d8
BuildTools <vikingiwan@gmail.com>
2017-03-11 08:47:59 -0500

    added debug log message for damage blocked by occultCrown

--- src/main/java/tk/vikingcorp/occultMod/OccultEventHandler.java
+++ src/main/java/tk/vikingcorp/occultMod/OccultEventHandler.java
@@ -1,10 +1,14 @@
 package tk.vikingcorp.occultMod;
 
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import tk.vikingcorp.occultMod.assist.LogHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
@@ -18,6 +22,10 @@ public class OccultEventHandler
 		if(base.getEquipmentInSlot(4) != null && base.getEquipmentInSlot(4).getItem() == ModItems.occultCrown)
 		{
 			event.setCanceled(true);
+			if (base instanceof EntityPlayerMP){
+				EntityPlayerMP player = (EntityPlayerMP) base;
+				LogHelper.debug("Damage for " + player.getDisplayName() + " blocked by occultCrown");
+			}
 		}
 	}
 }
